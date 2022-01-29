d646e3b4aa544c54318ede17f3cc0baa84b46681
eatmyvenom <eat.my.venomm@gmail.com>
2022-01-29 11:35:15 -0800

    feat(mongo): :card_file_box: add uuid to metadata

--- AccountMetadata.js
+++ AccountMetadata.js
@@ -1,9 +1,11 @@
 module.exports = class AccountMetadata {
   hacker = false;
   banned = false;
+  uuid = "";
 
-  constructor(hacker, banned) {
+  constructor(hacker, banned, uuid) {
     this.hacker = hacker;
     this.banned = banned;
+    this.uuid = uuid;
   }
 };
